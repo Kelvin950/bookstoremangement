1320e04b5e8c9a1d84ffd27a9d8a16468949e42f
kelvin <denlinato@gmail.com>
2023-10-09 01:11:53 +0000

    fixing errors

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
-	
+	// "encoding/json"
+	// "fmt"
+	// "os"
+	// "os/exec"
+	// "sync"
 
-	"log"
-	"net/http"
+	// _ "github.com/go-sql-driver/mysql"
+	// _ "github.com/gorilla/mux"
 
-	"github.com/gorilla/mux"
-	_ "github.com/jinzhu/gorm"
-	"github.com/kelvin950/bookStoreManagement/pkg/routes"
+
+"net/http"
+"log"
+"github.com/gorilla/mux"
+_ "github.com/jinzhu/gorm"
+"github.com/kelvin950/bookStoreManagement/pkg/routes"
 )
-
+// type pkg  struct{
+// 	Dir  string   `json:"dir"`
+//  ImportPath string  `json:"import_path"`
+//  Name  string			`json:"name"`
+//  Doc string		`json:"Doc"`
+//  Target string   	`json:"target"`
+//  Goroot  bool		`json:"goroot"`
+//  Standard  bool   `json:"standard"`
+//  Root string 		`json:"root"`
+//  GoFiles []string		`json:"gofiles"`
+//  Imports []string         `json:"imports"`
+//  Deps []string		`json:"deps"`
+// }
 func main(){
 
 	r :=  mux.NewRouter()
@@ -19,4 +38,42 @@ func main(){
 	http.Handle("/" , r)
 
 	log.Fatal(http.ListenAndServe(":8000" ,  r))
+
+// 	cmd :=  exec.Command("go" ,"list" , "-json" ,os.Args[1])
+
+// 	var x pkg 
+      
+	 
+//   output , err := cmd.Output() 
+ 
+//    if err!=nil{
+// 	panic(err)
+//    }
+
+//   json.Unmarshal(output , &x)
+	
+//  var wg sync.WaitGroup 
+
+//  wg.Add(len(x.Deps))
+//   for _ , r := range x.Deps  {
+
+// 	fmt.Println(r)
+
+// 	go func(r string){
+        
+// 		defer wg.Done()
+// 		output , err :=  exec.Command("go" ,"list" , "-json" ,r).Output()
+
+// 		if err!=nil{
+// 			panic(err)
+// 		}
+//              println("=======")
+// 		 println(string(output))
+// 		 println("===========")
+// 	}(r)
+//   }
+
+//   wg.Wait()
+   
+
 }
